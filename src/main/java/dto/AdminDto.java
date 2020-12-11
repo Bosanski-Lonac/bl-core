1c13c7a40bdfe4f3337c15671786e30772856b8f
Andrej Gasic <andrej@gasic.rs>
2020-12-11 14:35:06 +0100

    Uklanjanje slanje sifre admina klijentu

--- src/main/java/dto/AdminDto.java
+++ src/main/java/dto/AdminDto.java
@@ -6,19 +6,11 @@ import javax.validation.constraints.Size;
 public class AdminDto {
 	@NotBlank
 	private String username;
-	@Size(min = 5, message = "Sifra mora da bude bar 5 karaktera")
-	private String sifra;
 	
 	public String getUsername() {
 		return username;
 	}
 	public void setUsername(String username) {
 		this.username = username;
-	}
-	public String getSifra() {
-		return sifra;
-	}
-	public void setSifra(String sifra) {
-		this.sifra = sifra;
 	}
 }
